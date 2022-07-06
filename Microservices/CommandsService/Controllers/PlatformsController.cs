cd36f585e88aefe73aece3ad8fb8699422353089
Yoshimo94 <maciekprzygoda@interia.pl>
2022-07-06 18:15:34 +0200

    Add CommandsService project and connection between PlatformService and CommandsService using HttpClient

---
+++ Microservices/CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.Write("--> Inbound POST # Command Service");
+
+            return Ok("Inbound test of from Platforms Controller");
+        }
+    }
+}
